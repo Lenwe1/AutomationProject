d024006d71fe61f9cd96b25e3cc4e1dcad926d17
Lenwe1 <tibi.lionel@gmail.com>
2022-03-14 21:41:43 +0200

    Extra Structure Added

--- src/test/java/test/homepageTest/HomePageTest.java
+++ src/test/java/test/homepageTest/HomePageTest.java
@@ -8,6 +8,13 @@ public class HomePageTest extends BaseTest {
 
     @Test
     public void testLogoIsDisplayed(){
+
         Assert.assertTrue(homePage.checkLogo(), "Logo is not displayed");
+        homePage.clickSignInButton();
+        homePage.clickBack();
+        homePage.skipSignInButton();
+        homePage.clickBack();
+        homePage.checkEmailField("adresa@email.com");
+
     }
 }
